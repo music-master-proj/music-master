4906d517965dace876ed44fdff423da3d110dc8f
moram16 <moramihay@gmail.com>
2021-12-15 11:27:21 +0200

    changes server.js and organize pages

--- server.js
+++ server.js
@@ -1,88 +1,43 @@
-var SpotifyWebApi = require('spotify-web-api-node');
-const express = require('express')
+
+//Create server
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => { console.log(`Listening on port ${port}...`) });
 
 
-const scopes = [
-    'ugc-image-upload',
-    'user-read-playback-state',
-    'user-modify-playback-state',
-    'user-read-currently-playing',
-    'streaming',
-    'app-remote-control',
-    'user-read-email',
-    'user-read-private',
-    'playlist-read-collaborative',
-    'playlist-modify-public',
-    'playlist-read-private',
-    'playlist-modify-private',
-    'user-library-modify',
-    'user-library-read',
-    'user-top-read',
-    'user-read-playback-position',
-    'user-read-recently-played',
-    'user-follow-read',
-    'user-follow-modify'
-  ];
-  
-// credentials are optional
-var spotifyApi = new SpotifyWebApi({
-    clientId: '170be180d8704d1882bb27553cfbb068',
-    clientSecret: 'ae08d48e2bb0417f92941c4bf373bcc7',
-    redirectUri: 'http://localhost:8888/callback'
-  });
-  
-  const app = express();
-  
-  app.get('/login', (req, res) => {
-    res.redirect(spotifyApi.createAuthorizeURL(scopes));
-  });
-  
-  app.get('/callback', (req, res) => {
-    const error = req.query.error;
-    const code = req.query.code;
-    const state = req.query.state;
-  
-    if (error) {
-      console.error('Callback Error:', error);
-      res.send(`Callback Error: ${error}`);
-      return;
+const morgan = require('morgan');
+app.use(morgan("dev"));
+
+// for POST 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+//CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Origin-Headers","Origin,X- requested-With,Content-Type, Accept, Authorization");
+    if(req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Origin-Methods" , "PUT, POST, DELETE, GET");
+        return res.status(200).json({});
     }
-  
-    spotifyApi
-      .authorizationCodeGrant(code)
-      .then(data => {
-        const access_token = data.body['access_token'];
-        const refresh_token = data.body['refresh_token'];
-        const expires_in = data.body['expires_in'];
-  
-        spotifyApi.setAccessToken(access_token);
-        spotifyApi.setRefreshToken(refresh_token);
-  
-        console.log('access_token:', access_token);
-        console.log('refresh_token:', refresh_token);
-  
-        console.log(
-          `Sucessfully retreived access token. Expires in ${expires_in} s.`
-        );
-        res.send('Success! You can now close the window.');
-  
-        setInterval(async () => {
-          const data = await spotifyApi.refreshAccessToken();
-          const access_token = data.body['access_token'];
-  
-          console.log('The access token has been refreshed!');
-          console.log('access_token:', access_token);
-          spotifyApi.setAccessToken(access_token);
-        }, expires_in / 2 * 1000);
-      })
-      .catch(error => {
-        console.error('Error getting Tokens:', error);
-        res.send(`Error getting Tokens: ${error}`);
-      });
+    next();
   });
   
-  app.listen(8888, () =>
-    console.log(
-      'HTTP Server up. Now go to http://localhost:8888/login in your browser.'
-    )
-  )
+  //Error
+  app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+  })
+  
+  //middleware in case of - localhost:3000/dfnkjf
+  app.use((error,req,res, next)=> {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+  });
