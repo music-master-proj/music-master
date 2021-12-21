baa08f428ed0a5b5ea6b81ec8a6573e75015e4dc
moram16 <moramihay@gmail.com>
2021-12-21 16:05:34 +0200

    update

--- models/playlists.js
+++ models/playlists.js
@@ -1,30 +1,16 @@
+const mongoose =  require('mongoose');
 const { Schema, model } = require('mongoose');
 
 const PlaylistSchema = new Schema({
-    id: {
-        type: Number,
-        required: [true, 'id requierd']
-    },
-    name: {
-        type: String,
-        required: [true, 'company filed required']
-    },
-    description: {
-        type: String,
-        required: [true, 'destination filed required']
-    },
+    _id: mongoose.Schema.Types.ObjectId,
+    name: {type: String , require: true },
+    artist:  {type: String , require: true },
+    yearReleased: { type: Number , require: true , min: 1930, max: 2030},
+    duration: {type: Number , require: true, min: 0},
+    writer: { type: String ,require: true },
+    genre : { type: [String] ,enumValues:["Country", "Electronic dance", "Hip-hop", "Indie rock", "Jazz", "Metal", "Classic rock", "Rap", "Pop", "R&B", "Rock","Latin","reggaeton"], require: true},
+    image: {type: String, require: true },
     
-    date: {
-        type: Date,
-        required: [true, 'date required'],
-        min: ['2021-01-01', 'Must be greater than: 01/01/2021'],
-        max: ['2024-01-01', 'Must be smaller than: 01/01/2025'],
-    },
-
-    Image: {
-        type: String,
-        required: [true, 'destination filed required']
-    },
 }, { collection: 'Playlist' });
 
 const Playlist = model('Playlist', PlaylistSchema );
