62b6de3368d7c8e36e767bbfa1d32fefd6c57a6b
moram16 <moramihay@gmail.com>
2022-02-13 19:06:56 +0200

    Changing server and other files

--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./utils/express');
-const logger = require('./utils//logs');
-const { initConnection } = require('./utils//mongoose');
-const port = process.env.PORT || 8080;
-initConnection();
-
-app.listen(port , () => logger.info(`Lisining to Server : ${port}`));
