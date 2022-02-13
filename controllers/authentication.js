98ab48047cdee5ea2fa4a11fff1ae8d74a962984
moram16 <moramihay@gmail.com>
2022-02-13 16:33:17 +0200

    change

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -5,10 +5,7 @@ const bcrypt = require('bcrypt');
 let message = '';
 const url = require('url');
 
-function validateEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-};
+
 const checkEmail = function (req, res, next) {
     const queryObject = url.parse(req.url,true).query;
     if (req.body.email) {
