185f5f842d2b1d6785a9a2a3eb02882c1cedd3f5
guitit amar <79801261+guitit@users.noreply.github.com>
2022-02-16 16:21:52 +0200

    delete server

--- server/src/routes/calendarRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router(),
-  calendar = require('../controller/Calendar').calendarController();
-
-router.put('/', calendar.upsertCalendar)      // upsert the calendar
-router.get('/:userID', calendar.userCalendar);   // get calendar through user id
-
-module.exports = router
